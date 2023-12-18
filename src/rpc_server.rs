ceb495158058c2fe344ccaed4b092c698f7435a1
danda <dan-da@users.noreply.github.com>
2023-12-17 18:06:43 -0800

    style: use Atomic get/set methods `syncing`
    Uses the new get/set methods for `syncing: AtomicRw<bool>`.
    
    Using these methods make it certain that the locking is encapsulated
    inside `syncing` and there is no chance of a lock ordering deadlock.
    
    It also just looks cleaner.

--- src/rpc_server.rs
+++ src/rpc_server.rs
@@ -330,7 +330,7 @@ impl RPC for NeptuneRPCServer {
     ) -> DashBoardOverviewDataFromClient {
         let tip_header = executor::block_on(self.state.chain.light_state.get_latest_block_header());
         let wallet_status = executor::block_on(self.state.get_wallet_status_for_tip());
-        let syncing = self.state.net.syncing.lock(|s| *s);
+        let syncing = self.state.net.syncing.get();
         let mempool_size = self.state.mempool.get_size();
         let mempool_tx_count = self.state.mempool.len();
 
