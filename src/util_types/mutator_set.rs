793a5fc1252f21ee8ae6abb9e3f3e802a4cba9fd
danda <dan-da@users.noreply.github.com>
2024-03-20 08:55:00 -0700

    wip. add MmrAsync trait

--- src/util_types/mutator_set.rs
+++ src/util_types/mutator_set.rs
@@ -5,6 +5,7 @@ pub mod archival_mutator_set;
 pub mod boxed_big_array;
 pub mod chunk;
 pub mod chunk_dictionary;
+pub mod mmr_trait_async;
 pub mod mmra_and_membership_proofs;
 pub mod ms_membership_proof;
 pub mod msa_and_records;
