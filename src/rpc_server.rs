aa8da171172831fed86cef0245ef355c4e108397
danda <dan-da@users.noreply.github.com>
2024-12-05 15:22:29 -0800

    wip. known_keys rpcs now return iterators instead of Vec

--- src/rpc_server.rs
+++ src/rpc_server.rs
@@ -50,7 +50,7 @@ use crate::models::state::tx_proving_capability::TxProvingCapability;
 use crate::models::state::wallet::address::encrypted_utxo_notification::EncryptedUtxoNotification;
 use crate::models::state::wallet::address::KeyType;
 use crate::models::state::wallet::address::ReceivingAddress;
-use crate::models::state::wallet::address::SpendingKey;
+use crate::models::state::wallet::address::SpendingKeyRangeIter;
 use crate::models::state::wallet::coin_with_possible_timelock::CoinWithPossibleTimeLock;
 use crate::models::state::wallet::expected_utxo::UtxoNotifier;
 use crate::models::state::wallet::monitored_utxo::MonitoredUtxo;
@@ -220,10 +220,10 @@ pub trait RPC {
     async fn next_receiving_address(key_type: KeyType) -> ReceivingAddress;
 
     /// Return all known keys, for every [KeyType]
-    async fn known_keys() -> Vec<SpendingKey>;
+    async fn known_keys() -> Vec<SpendingKeyRangeIter>;
 
     /// Return known keys for the provided [KeyType]
-    async fn known_keys_by_keytype(key_type: KeyType) -> Vec<SpendingKey>;
+    async fn known_keys_by_keytype(key_type: KeyType) -> SpendingKeyRangeIter;
 
     /// Return the number of transactions in the mempool
     async fn mempool_tx_count() -> usize;
@@ -1069,14 +1069,15 @@ impl RPC for NeptuneRPCServer {
     }
 
     // documented in trait. do not add doc-comment.
-    async fn known_keys(self, _context: tarpc::context::Context) -> Vec<SpendingKey> {
+    async fn known_keys(self, _context: tarpc::context::Context) -> Vec<SpendingKeyRangeIter> {
         log_slow_scope!(fn_name!());
 
         self.state
             .lock_guard()
             .await
             .wallet_state
-            .get_all_known_spending_keys()
+            .known_spending_keys_iter()
+            .await
             .collect()
     }
 
@@ -1085,15 +1086,15 @@ impl RPC for NeptuneRPCServer {
         self,
         _context: tarpc::context::Context,
         key_type: KeyType,
-    ) -> Vec<SpendingKey> {
+    ) -> SpendingKeyRangeIter {
         log_slow_scope!(fn_name!());
 
         self.state
             .lock_guard()
             .await
             .wallet_state
-            .get_known_spending_keys(key_type)
-            .collect()
+            .known_spending_keys_by_keytype_iter(key_type)
+            .await
     }
 
     // documented in trait. do not add doc-comment.
