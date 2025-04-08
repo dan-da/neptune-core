fe8e4135ced9c7cd7777670c6d39d611849ba093
danda <dan-da@users.noreply.github.com>
2025-04-07 18:29:28 -0700

    refactor: move network into TransactionDetails
    addresses #544 code review.
    
    adds network field to TransactionDetails and removes it from
    TxCreationArtifacts.
    
    adapts callers to match.
    
    in response to:
    https://github.com/Neptune-Crypto/neptune-core/pull/544#discussion_r2030182752

--- src/api/tx_initiation/send.rs
+++ src/api/tx_initiation/send.rs
@@ -148,7 +148,6 @@ impl TransactionSender {
         let tx_creation_artifacts = TxCreationArtifactsBuilder::new()
             .transaction_details(tx_details)
             .transaction(transaction)
-            .network(gsl.cli().network)
             .build()?;
 
         tracing::info!("send: recording tx");
